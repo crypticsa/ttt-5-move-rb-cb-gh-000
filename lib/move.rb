82e4777adc39d93e4b6ab29e1cd37c42cc3d544f
Dusto´n Lomrt <simon.m.f.w.knott@eclipso.de>
2019-01-03 15:03:59 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,7 +5,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-def input_to_index
+def input_to_index(user_input)
   converted_input = user_input.to_i - 1
 end
 # code your input_to_index and move method here!
